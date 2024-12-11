ec0952f95abfcc4ced85e8d917190e141d021347
uid0Z0uh4iri <mohamedzouhairi023@gmail.com>
2024-12-11 21:05:48 +0100

    Delete Update_Table_Projets.sql

--- Update_Table_Projets.sql
+++
@@ -1,8 +0,0 @@
-USE itthink;
--- Mise à jour des détails d’un projet
-UPDATE Projets
-SET titre_projet = 'Site E-commerce Pro', description = 'Site complet avec système de paiement'
-WHERE id_projet = 1;
-
-
-SELECT * FROM projets;

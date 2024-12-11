26761d873b51d08b31ca1223e65fd07c7624bff0
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2024-12-11 14:41:58 +0100

    Creating Database & Creating Tables

---
+++ Creating_DB_TABLES.sql
@@ -0,0 +1,67 @@
+-- Création de la base de données
+CREATE DATABASE ITThink;
+USE ITThink;
+
+-- Table Utilisateurs
+CREATE TABLE Utilisateurs (
+    id_utilisateur INT AUTO_INCREMENT PRIMARY KEY,
+    nom_utilisateur VARCHAR(255) NOT NULL,
+    mot_de_passe VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    autres_informations TEXT
+);
+
+CREATE TABLE Categories (
+    id_categorie INT AUTO_INCREMENT PRIMARY KEY,
+    nom_categorie VARCHAR(255) NOT NULL
+);
+
+-- Table Sous-Catégories
+CREATE TABLE Sous_Categories (
+    id_sous_categorie INT AUTO_INCREMENT PRIMARY KEY,
+    nom_sous_categorie VARCHAR(255) NOT NULL,
+    id_categorie INT,
+    FOREIGN KEY (id_categorie) REFERENCES Categories(id_categorie)
+);
+
+-- Table Projets
+CREATE TABLE Projets (
+    id_projet INT AUTO_INCREMENT PRIMARY KEY,
+    titre_projet VARCHAR(255) NOT NULL,
+    description TEXT,
+    id_categorie INT,
+    id_sous_categorie INT,
+    id_utilisateur INT,
+    FOREIGN KEY (id_categorie) REFERENCES Categories(id_categorie),
+    FOREIGN KEY (id_sous_categorie) REFERENCES Sous_Categories(id_sous_categorie),
+    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateurs(id_utilisateur)
+);
+
+-- Table Freelances
+CREATE TABLE Freelances (
+    id_freelance INT AUTO_INCREMENT PRIMARY KEY,
+    nom_freelance VARCHAR(255) NOT NULL,
+    competences TEXT,
+    id_utilisateur INT,
+    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateurs(id_utilisateur)
+);
+
+-- Table Offres
+CREATE TABLE Offres (
+    id_offre INT AUTO_INCREMENT PRIMARY KEY,
+    montant DECIMAL(10, 2) NOT NULL,
+    delai INT NOT NULL,
+    id_freelance INT,
+    id_projet INT,
+    FOREIGN KEY (id_freelance) REFERENCES Freelances(id_freelance),
+    FOREIGN KEY (id_projet) REFERENCES Projets(id_projet)
+);
+
+-- Table Témoignages
+CREATE TABLE Temoignages (
+    id_temoignage INT AUTO_INCREMENT PRIMARY KEY,
+    commentaire TEXT,
+    id_utilisateur INT,
+    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateurs(id_utilisateur)
+);
+
